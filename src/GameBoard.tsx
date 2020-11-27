affd78aaf4d307e43f98ec38aa6eac5e515e8112
Justin Behnke <justin@databased.com>
2020-11-27 01:39:58 -0700

    Add simple game play layout

---
+++ src/GameBoard.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import PlayerLayout from "./PlayerLayout";
+
+class GameBoard extends Component {
+  render() {
+    return (
+      <div>
+        <PlayerLayout />
+      </div>
+    );
+  }
+}
+
+export default GameBoard;
